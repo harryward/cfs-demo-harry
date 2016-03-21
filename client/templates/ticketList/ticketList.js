ed46bea234e12ca47d0cfd8281f043d3b58770d7
Harry Ward <harry@townsquaredigital.com>
2016-03-21 13:04:33 -0600

    delete button

--- client/templates/ticketList/ticketList.js
+++ client/templates/ticketList/ticketList.js
@@ -26,6 +26,12 @@ Template.ticketList.events({
     'click .editMe':function(event,template){
         event.preventDefault();
         alert('make this button go to a route that lets you edit the files, title and summary')
+    },
+    'click .removeMe':function(event,template){
+        event.preventDefault();
+        if(confirm('are you sure? this cannot be reversed')){
+        Tickets.remove(this._id)
+        }
     }
 });
 
