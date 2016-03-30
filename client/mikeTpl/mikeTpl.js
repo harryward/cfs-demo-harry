4ce9be11c21207f66f6054a909e05daf95c77e84
Wolstat <mwolstat@gmail.com>
2016-03-30 15:54:52 -0400

    mikes filter cleanup

---
+++ client/mikeTpl/mikeTpl.js
@@ -0,0 +1,41 @@
+Template.mikeTpl.helpers({
+    //add you helpers here
+    'rawFilter': function(){
+        return EJSON.stringify( Session.get('filterQuery'), {'indent':true } );
+    }
+
+});
+
+Template.mikeTpl.events({
+    'submit .advancedFilterForm': function(event, template){
+        event.preventDefault();
+        var formObj = {};
+        var fields = template.findAll('.form-control');
+        _.each(fields, function(e, i){
+            if(e.name) {
+                formObj[e.name] = e.value;
+            }
+        })
+        console.log("form data: ", formObj);
+
+        formObj.date = new Date();
+        if(Session.get('searchQuery')){
+            formObj.s_query = Session.get('searchQuery');
+        }
+
+        Session.set('filterQuery', formObj);
+    }
+});
+
+Template.mikeTpl.onCreated(function () {
+    //add your statement here
+});
+
+Template.mikeTpl.onRendered(function () {
+    //add your statement here
+});
+
+Template.mikeTpl.onDestroyed(function () {
+    //add your statement here
+});
+
